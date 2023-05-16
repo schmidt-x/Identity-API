ae9cafcc3d5d7d9157185179f1aa392d99b95fe3
Schmidt <Ukrainenko95@gmail.com>
2023-05-16 14:10:10 +0300

    feat: add data access layer

---
+++ lib/IdentityLibrary/Data/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using IdentityLibrary.Contracts.Options;
+using Microsoft.Extensions.Options;
+
+namespace IdentityLibrary.Data.DataAccess;
+
+public class SqlDataAccess : ISqlDataAccess
+{
+	private readonly ConnectionStringsOptions _connStrings;
+	
+	public SqlDataAccess(IOptions<ConnectionStringsOptions> options)
+	{
+		_connStrings = options.Value;
+	}
+	
+	private SqlConnection GetConnection() =>
+		new SqlConnection(_connStrings.Mssql);
+	
+	
+	public async Task<IEnumerable<TResult>> LoadData<TResult>(string sql, DynamicParameters parameters, CancellationToken token)
+	{
+		await using var cnn = GetConnection();
+		
+		return await cnn.QueryAsync<TResult>(new CommandDefinition(sql, parameters, cancellationToken: token));
+	}
+	
+	public async Task<TResult> LoadSingle<TResult>(string sql, DynamicParameters parameters, CancellationToken token)
+	{
+		await using var cnn = GetConnection();
+		
+		return await cnn.QueryFirstOrDefaultAsync<TResult>(new CommandDefinition(sql, parameters, cancellationToken: token));
+	}
+	
+	public async Task<TResult> LoadScalar<TResult>(string sql, DynamicParameters parameters, CancellationToken token)
+	{
+		await using var cnn = GetConnection();
+		
+		return await cnn.ExecuteScalarAsync<TResult>(new CommandDefinition(sql, parameters, cancellationToken: token));
+	}
+
+	public async Task SaveData(string sql, DynamicParameters parameters, CancellationToken token)
+	{
+		await using var cnn = GetConnection();
+		
+		await cnn.ExecuteAsync(new CommandDefinition(sql, parameters, cancellationToken: token));
+	}
+}
