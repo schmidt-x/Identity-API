4caba69c986f227e699c02e1968523b55fc23070
Schmidt <schmidtsigma@gmail.com>
2023-07-03 20:41:58 +0300

    refactor: delete UserExistsResult

--- src/IdentityApi/Results/UserExistsResult.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace IdentityApi.Results;
-
-public class UserExistsResult
-{
-	public bool Exists { get; set; }
-	public Dictionary<string, IEnumerable<string>>? Errors { get; set; }
-}
