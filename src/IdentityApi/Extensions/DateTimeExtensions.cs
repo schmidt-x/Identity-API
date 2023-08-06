fc798668fde1e706e2dbadc42104a55150d87967
Schmidt <schmidtsigma@gmail.com>
2023-08-06 18:19:53 +0300

    feat: add DateTime extension method

---
+++ src/IdentityApi/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityApi.Extensions;
+
+public static class DateTimeExtensions
+{
+	public static long GetTotalSeconds(this DateTime now)
+	{
+		var start = new DateTime(1970, 1, 1, 0, 0, 0);
+		return (now.Ticks - start.Ticks) / 10_000_000;
+	}
+}
