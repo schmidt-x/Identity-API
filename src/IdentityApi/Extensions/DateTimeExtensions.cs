02f8c57a1534056c4d2537e72eaf14e2bb7215ac
Schmidt <schmidtsigma@gmail.com>
2023-08-09 11:59:55 +0300

    docs: document extension method GetTotalSeconds

--- src/IdentityApi/Extensions/DateTimeExtensions.cs
+++ src/IdentityApi/Extensions/DateTimeExtensions.cs
@@ -4,6 +4,11 @@ namespace IdentityApi.Extensions;
 
 public static class DateTimeExtensions
 {
+	/// <summary>
+	/// Calculates the tolal number of seconds that have passed since the Unix epoch (1970.01.01 00:00:00)
+	/// </summary>
+	/// <param name="now">The <see cref="DateTime"/> relative to which to calculate</param>
+	/// <returns>The total number of seconds since the Unix epoch</returns>
 	public static long GetTotalSeconds(this DateTime now)
 	{
 		var start = new DateTime(1970, 1, 1, 0, 0, 0);
